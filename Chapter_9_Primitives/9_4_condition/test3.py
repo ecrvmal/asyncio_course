0e20b14d8e83ee0f3869a0332d284f436c18f8f6
Мальчевский Владимир <malchevskii@T8.CORP>
2024-02-27 10:17:26 +0300

    9_4 test3 not done

--- Chapter_9_Primitives/9_4_condition/test3.py
+++ Chapter_9_Primitives/9_4_condition/test3.py
@@ -4,12 +4,7 @@ import asyncio
 
 
 
-stone_condition = asyncio.Condition()
-stone_completed = asyncio.Event()
-metal_condition = asyncio.Condition()
-metal_completed = asyncio.Event()
-cloth_condition = asyncio.Condition()
-cloth_completed = asyncio.Event()
+
 stone_material = 0
 metal_material = 0
 cloth_material = 0
@@ -19,10 +14,8 @@ cloth_items = 0
 
 
 
-async def gather_stone():
+async def gather_stone(stone_condition, stone_completed):
     # Добываем камень, 10ед каждую сек.
-    global stone_condition
-    global stone_completed
     global stone_material
     print(f'Raw stone started')
     while True:
@@ -38,10 +31,8 @@ async def gather_stone():
 
 
 
-async def gather_metal():
+async def gather_metal(metal_condition, metal_completed):
     # Добываем металл, 6ед каждую сек.
-    global metal_condition
-    global metal_completed
     global metal_material
     print(f'Raw metal started')
     while True:
@@ -50,16 +41,14 @@ async def gather_metal():
             return
         async with metal_condition:
             await asyncio.sleep(1)
-            metal_material += 10
+            metal_material += 6
             print (f'Metal stock: {metal_material} units')
             metal_condition.notify_all()
             await metal_condition.wait()
 
 
-async def gather_cloth():
+async def gather_cloth(cloth_condition, cloth_completed):
     # Добываем ткань, 8ед каждую сек.
-    global cloth_condition
-    global cloth_completed
     global cloth_material
     print(f'Raw cloth started')
     while True:
@@ -69,17 +58,15 @@ async def gather_cloth():
         async with cloth_condition:
             await asyncio.sleep(1)
             cloth_material += 8
-            print (f'cloth stock: {cloth_material} units')
+            print(f'cloth stock: {cloth_material} units')
             cloth_condition.notify_all()
             await cloth_condition.wait()
 
 
-async def craft_stone_items(item, qty):
+async def craft_stone_items(stone_condition, stone_completed, item, qty):
     # Мастерская по крафту из камня
     global stone_items
-    global stone_condition
     global stone_material
-    global stone_completed
     print(f'Stone order for {item}, need {qty}')
     while True:
         async with stone_condition:
@@ -87,7 +74,7 @@ async def craft_stone_items(item, qty):
                 await stone_condition.wait()
                 print(f'item {item} was produced')
                 stone_material -= qty
-                stone_items +=1
+                stone_items += 1
                 if stone_items == 3:
                     stone_completed.set()
                     print(f'all stone goods were produced')
@@ -95,18 +82,15 @@ async def craft_stone_items(item, qty):
             stone_condition.notify()
 
 
-
-async def craft_metal_items(item, qty):
+async def craft_metal_items(metal_condition, metal_completed, item, qty):
     # Мастерская по крафту из мателла
     global metal_items
-    global metal_condition
     global metal_material
-    global metal_completed
     print(f'Metal order for {item}, need {qty}')
     while True:
         async with metal_condition:
             if metal_material >= qty:
-                await stone_condition.wait()
+                await metal_condition.wait()
                 print(f'item {item} was produced')
                 metal_material -= qty
                 metal_items += 1
@@ -117,12 +101,10 @@ async def craft_metal_items(item, qty):
             metal_condition.notify_all()
 
 
-async def craft_cloth_items(item, qty):
+async def craft_cloth_items(cloth_condition, cloth_completed, item, qty):
     # Мастерская по крафту из ткани
     global cloth_items
-    global cloth_condition
     global cloth_material
-    global cloth_completed
     print(f'Cloth order for {item}, need {qty}')
     while True:
         async with cloth_condition:
@@ -139,6 +121,14 @@ async def craft_cloth_items(item, qty):
 
 
 async def main():
+    stone_condition = asyncio.Condition()
+    stone_completed = asyncio.Event()
+    metal_condition = asyncio.Condition()
+    metal_completed = asyncio.Event()
+    cloth_condition = asyncio.Condition()
+    cloth_completed = asyncio.Event()
+
+
     # Запускаем производства
     stone_resources_dict = {
         'Каменная плитка': 10,
@@ -158,9 +148,9 @@ async def main():
         'Тканевое покрывало': 48,
     }
 
-    stone_goods = [asyncio.create_task(craft_stone_items(name, qty)) for name, qty in stone_resources_dict.items()]
-    metal_goods = [asyncio.create_task(craft_metal_items(name, qty)) for name, qty in metal_resources_dict.items()]
-    cloth_goods = [asyncio.create_task(craft_cloth_items(name, qty)) for name, qty in cloth_resources_dict.items()]
+    stone_goods = [asyncio.create_task(craft_stone_items(stone_condition, stone_completed, name, qty)) for name, qty in stone_resources_dict.items()]
+    metal_goods = [asyncio.create_task(craft_metal_items(metal_condition, metal_completed, name, qty)) for name, qty in metal_resources_dict.items()]
+    cloth_goods = [asyncio.create_task(craft_cloth_items(cloth_condition, cloth_completed, name, qty)) for name, qty in cloth_resources_dict.items()]
 
     await asyncio.gather(gather_stone(), gather_metal(), gather_cloth(),
                          *stone_goods, *metal_goods, *cloth_goods)
