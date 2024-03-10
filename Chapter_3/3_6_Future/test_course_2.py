fa9d9b628527db7d7df4e8a8544b3e5a0ff6c3ae
ecrvmal1 <vladimir.malchevskiy@gmail.com>
2024-03-10 22:29:02 +0300

    polling completed

--- Chapter_3/3_6_Future/test_course_2.py
+++ Chapter_3/3_6_Future/test_course_2.py
@@ -4,15 +4,8 @@ import asyncio
 
 async def async_operation():
     print("Начало асинхронной операции.")
-    # await asyncio.sleep(2)
-    # print("Асинхронная операция успешно завершилась.")
-    try:
-        await asyncio.sleep(2)
-        print("Асинхронная операция успешно завершилась.")
-    except asyncio.CancelledError:
-        print("Асинхронная операция была отменена в процессе выполнения.")
-        # Вновь поднимаем исключение, чтобы передать его дальше
-        raise
+    await asyncio.sleep(2)
+    print("Асинхронная операция успешно завершилась.")
 
 
 async def main():
@@ -23,9 +16,12 @@ async def main():
     # Отменяем Future до его завершения
     future.cancel()
     try:
-        result = future.result()
+        result = await future
         print("Результат Future:", result)
-    except:
+    except asyncio.CancelledError:
+        print("Асинхронная операция была отменена в процессе выполнения.")
+        # Вновь поднимаем исключение, чтобы передать его дальше
+        # raise
         print("Обработка исключения: Future был отменен.")
 
     if future.cancelled():
@@ -34,10 +30,6 @@ async def main():
         print("Проверка: Future не был отменен.")
 
     print("Главная корутина завершена.")
-
-
-
-
 
 
 asyncio.run(main())
